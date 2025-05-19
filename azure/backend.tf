00e50c866d1dbec889f5b82145e97b70a89f935f
Pratyushaa94 <prathyusha9449@gmail.com>
2025-05-19 13:16:14 +0530

    backend

--- azure/backend.tf
+++ azure/backend.tf
@@ -1,9 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "resource_group_name"
-        storage_account_name = "demoterraformstate"
-    container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+    resource_group_name   = "myResourceGroup"
+    storage_account_name  = "demostorageacct1234"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
   }
 }
-
