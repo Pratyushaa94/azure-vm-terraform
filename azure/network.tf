7346b713afa2126fd7d6340fa856d3ceacf9faf6
Pratyushaa94 <prathyusha9449@gmail.com>
2025-05-19 14:23:27 +0530

    changed networktf

--- azure/network.tf
+++ azure/network.tf
@@ -18,3 +18,35 @@ resource "azurerm_subnet" "subnet_private" {
   resource_group_name  = var.resource_group_name
   address_prefixes     = ["10.0.2.0/24"]
 }
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "PublicIP"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "public_nic" {
+  name                = "PublicNIC"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet_public.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+resource "azurerm_network_interface" "private_nic" {
+  name                = "PrivateNIC"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet_private.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
