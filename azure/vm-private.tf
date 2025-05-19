ca7e002964ce8897a5b7f2e1787ed08fe860aeb5
Pratyushaa94 <prathyusha9449@gmail.com>
2025-05-19 09:05:36 +0530

    changes

--- azure/vm-private.tf
+++ azure/vm-private.tf
@@ -1,31 +1,26 @@
-resource "azurerm_virtual_machine" "vm_private" {
-  name                  = "Demo-PrivateVM"
-  location              = var.location
-  resource_group_name   = var.resource_group_name
-  vm_size               = var.vm2_private_size
-  network_interface_ids = [azurerm_network_interface.nic_private.id]
+resource "azurerm_linux_virtual_machine" "private_vm" {
+  name                = "Demo-PrivateVM"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = var.vm2_private_size
+  admin_username      = "azureuser"
 
-  os_profile {
-    computer_name  = "vm-private"
-    admin_username = "adminuser"
-    admin_password = "SecurePass123!"
+  network_interface_ids = [azurerm_network_interface.private_nic.id]
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
   }
 
-  os_profile_linux_config {
-    disable_password_authentication = false
+  os_disk {
+    caching               = "ReadWrite"
+    storage_account_type  = "Standard_LRS"
   }
 
-  storage_os_disk {
-    name              = "vm-private-osdisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  storage_image_reference {
+  source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
+    sku       = "22_04-lts"
     version   = "latest"
   }
 }
